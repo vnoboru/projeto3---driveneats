e0a2bc49bf7335f5df76626658595b1ecd5c5918
Vitor Torii <110076508+vnoboru@users.noreply.github.com>
2022-08-15 15:15:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-let nameDish, nameDrink, nameDessert, priceDish, priceDrink, priceDessert, price1, price2, price3, p1, p2, p3, total_cost;
+let nameDish, nameDrink, nameDessert, priceDish, priceDrink, priceDessert, price1, price2, price3, p1, p2, p3;
 
-function oneDish(optionDish){    
-    nameDish = optionDish.querySelector("h4").innerHTML;
-    priceDish = optionDish.querySelector(".text > .price > strong").innerHTML;
+function oneDish(optionDish){
+    nameDish = optionDish.querySelector("h3").innerHTML;
+    priceDish = optionDish.querySelector("strong").innerHTML;
     price1 = Number(priceDish.replace(",", "."));
 
     const markedDish = document.querySelector(".selectedDish");
 
     if(markedDish !== null){
         markedDish.classList.remove("selectedDish");
-    } 
+    }
 
     optionDish.classList.add("selectedDish");
     enableButton();
 }
 
 function oneDrink(optionDrink){
-    nameDrink = optionDrink.querySelector("h4").innerHTML;
-    priceDrink = optionDrink.querySelector(".text > .price > strong").innerHTML;
+    nameDrink = optionDrink.querySelector("h3").innerHTML;
+    priceDrink = optionDrink.querySelector("strong").innerHTML;
     price2 = Number(priceDrink.replace(",","."));
 
     const markedDrink = document.querySelector(".selectedDrink");
@@ -30,8 +30,8 @@ function oneDrink(optionDrink){
 }
 
 function oneDessert(optionDessert){
-    nameDessert = optionDessert.querySelector("h4").innerHTML;    
-    priceDessert = optionDessert.querySelector(".text > .price > strong").innerHTML;
+    nameDessert = optionDessert.querySelector("h3").innerHTML;
+    priceDessert = optionDessert.querySelector("strong").innerHTML;
     price3 = Number(priceDessert.replace(",","."));
 
     const markedDessert = document.querySelector(".selectedDessert");
@@ -43,25 +43,25 @@ function oneDessert(optionDessert){
     enableButton();
 }
 
-function enableButton(){   
+function enableButton(){
     const markedDish = document.querySelector(".selectedDish");
     const markedDrink = document.querySelector(".selectedDrink");
     const markedDessert = document.querySelector(".selectedDessert");
 
     if(markedDish !== null && markedDrink !== null && markedDessert !== null) {    
                 const lastOrder = document.querySelector(".button_finish");
-                lastOrder.innerHTML = "Fechar pedido"
+                lastOrder.innerHTML = "Fechar pedido";
                 lastOrder.classList.add("button");
-    }   
+    }
 }
 
 function finish(){
-    const total_cost = price1 + price2 + price3;
+    const totalCost = price1 + price2 + price3;
     const dish = String(nameDish);
     const drink = String(nameDrink);
     const dessert = String(nameDessert);
 
-    const request = `Olá, gostaria de fazer o pedido:\n- Prato: ${dish}\n- Bebida: ${drink}\n- Sobremesa: ${dessert}\nTotal: R$ ${total_cost.toFixed(2)}`;
+    const request = `Olá, gostaria de fazer o pedido:\n- Prato: ${dish}\n- Bebida: ${drink}\n- Sobremesa: ${dessert}\nTotal: R$ ${totalCost.toFixed(2)}`;
 
     window.open(`https://wa.me/5544999440023/?text=${encodeURIComponent(request)}`);
-  }
+}
